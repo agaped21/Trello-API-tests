5737cfe1d0830aac971fa04ee0c5f41daec5fc20
agaped21 <aga.pedlewska@gmail.com>
2025-07-30 15:36:32 +0200

    chore: add secrets upload from config.properties and add config.properties to .gitignore

---
+++ src/test/java/utils/Secrets.java
@@ -0,0 +1,22 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Secrets {
+
+    public static String get(String keyName) {
+        String env = System.getenv(keyName);
+        if (env != null && !env.isEmpty()) return env;
+
+        Properties props = new Properties();
+        try (InputStream input = new FileInputStream("src/test/resources/config.properties")) {
+            props.load(input);
+            return props.getProperty(keyName);
+        } catch (IOException e) {
+            throw new RuntimeException("Nie udało się załadować sekretu: " + keyName);
+        }
+    }
+}
