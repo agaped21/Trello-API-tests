89f3db14db331ccb8967c2a6496f7f1f8aed12e8
agaped21 <aga.pedlewska@gmail.com>
2025-07-31 15:31:51 +0200

    refactor: read baseUri from config instead of hardcoded value

--- src/test/java/api/clients/BoardClient.java
+++ src/test/java/api/clients/BoardClient.java
@@ -1,5 +1,6 @@
 package api.clients;
 
+import api.utils.Config;
 import api.utils.Secrets;
 import io.restassured.response.Response;
 
@@ -9,12 +10,14 @@ public class BoardClient {
 
     private final String trelloKey = Secrets.get("TRELLO_KEY");
     private final String trelloToken = Secrets.get("TRELLO_TOKEN");
+    private final String trelloBaseUri = Config.get("TRELLO_BASE_URI");
+    private final String trelloBasePath = "/1/boards/";
 
     public Response getBoard(String boardId) {
 
         return given()
-                .baseUri("https://api.trello.com")
-                .basePath("/1/boards/" + boardId)
+                .baseUri(trelloBaseUri)
+                .basePath(trelloBasePath + boardId)
                 .queryParam("key", trelloKey)
                 .queryParam("token", trelloToken)
                 .when().get();
